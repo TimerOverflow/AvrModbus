2420564b49bf64ce91f4bea906ddfd61f266347d
TimerOverflow <38400bps@gmail.com>
2021-09-14 22:39:47 +0900

    1. SLAVE:: fix bug AvrModbusSlaveProc()

--- AvrModbus.h
+++ AvrModbus.h
@@ -9,10 +9,16 @@
 /*********************************************************************************/
 #include "AvrUart.h"
 /*********************************************************************************/
-#define AVR_MODBUS_REVISION_DATE				20190319
+#define AVR_MODBUS_REVISION_DATE				20190416
 /*********************************************************************************/
 /** REVISION HISTORY **/
 /*
+	2019. 04. 16.					- Slave파트 AvrModbusSlaveProc()함수에서 CRC에러 코드 응답할 때 유효한 펑션코드가 수신 되었는지 검사 추가.
+	Jeong Hyun Gu						(프레임 인식에 오류 발견, A슬레이브 응답 중 B슬레이브가 ID패킷을 수신하지 못 하고 우연히 B슬레이브를 호출하는 것으로 인식하여
+													동작. 결과적으로 B슬레이브가 CRC에러 응답을 하여 정상응답 패킷과 프레임 충돌 발생. 근본적인 원인 파악이 필요하나 시간관계상
+													위와 같은 임시조치 추가. 이러한 이슈는 본 모듈을 사용한 슬레이브가 2대 이상일 경우 발생 가능성 있음. 추후 확인 필요.)
+												- AVR_MODBUS_RECEIVING_DELAY_US 값 '50000' -> '20000' 변경.
+
 	2019. 03. 19.					-	tag_AvrModbusMasterCtrl::SlaveReceive 삭제.
 	Jeong Hyun Gu					- AvrModbusMasterAddSlavePollData() 추가.
 													이제 마스터는 슬레이브 데이터를 비선형적으로 호출 가능. 예를 들어 200~210번지, 260~280번지등
@@ -92,9 +98,9 @@
 #define	null		0
 
 #define	AVR_MODBUS_MASTER			true
-#define	AVR_MODBUS_SLAVE			false
+#define	AVR_MODBUS_SLAVE			true
 
-#define AVR_MODBUS_RECEIVING_DELAY_US							50000
+#define AVR_MODBUS_RECEIVING_DELAY_US							20000
 #define AVR_MODBUS_DEFAULT_POLLING_DELAY_US				500000
 #define AVR_MODBUS_DEFAULT_SLAVE_NO_RESPONSE			20
 
